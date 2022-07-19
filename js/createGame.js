b721de3ff3c9a86c29945a6689a0c3750b92bc04
Masoud <mas.sam1368@gmail.com>
2022-07-19 14:52:17 +0430

    second

--- js/createGame.js
+++ js/createGame.js
@@ -1,19 +1,30 @@
+import createRandomArray from "./createRandomArray.js"
+
 export default function createGame(x_grid, y_grid) {
+  const blockNum = x_grid * y_grid
   const gameEl = document.getElementById('game')
 
   gameEl.innerHTML = ''
 
-  for(let i = 1 ; i <= x_grid * y_grid  ; i++) {
+  const randomArray = createRandomArray(blockNum)
+
+  for(let i = 1 ; i <= blockNum ; i++) {
+
     const element = document.createElement('div')
     const span = document.createElement('span')
 
-    span.innerText = i
+    if(i === blockNum) {
+      
 
-    element.classList.add('item')
+    } else {
 
-    element.style.cssText = `width: ${100/x_grid}%; height: ${100/y_grid}%`
+      span.innerText = i
 
-    element.appendChild(span)
+      element.classList.add('item')
+      element.style.cssText = `width: ${100/x_grid}%; height: ${100/y_grid}%; order: ${randomArray[i]}`
+      element.appendChild(span)
+
+    }
 
     gameEl.appendChild(element)
   }
---
+++ js/createRandomArray.js
@@ -0,0 +1,19 @@
+export default function createRandomArray(memberNum) {
+ 
+  const nums = []
+  for(let i = 1; i <= memberNum; i++) {
+    nums.push(i)
+  }
+
+  const ranNums = []
+  let i = nums.length
+  let j = 0
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i+1));
+    ranNums.push(nums[j]);
+    nums.splice(j,1);
+  }
+  
+  return ranNums
+}
